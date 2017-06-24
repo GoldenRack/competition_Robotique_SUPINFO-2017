d0a3ab0948e9f4dec9c4244c0791bb5bc6e32895
Alban Durrheimer <alban6800@hotmail.fr>
2017-06-24 14:56:46 +0200

    Add files via upload

--- main.ino
+++ main.ino
@@ -1,9 +1,25 @@
+#include <Adafruit_TCS34725.h>
 #include <Moteur.h>
-#include <parking.ino>
-#include <global.ino>
+#include <Servo.h> 
 
-//Initialise le mode des pins
-void setup(){
+//Définition des pins triggers et echos pour les sonars
+int SonarTriggerAvant = 26;
+int SonarTriggerArriere = 32;
+int SonarTriggerDroite = 22;
+int SonarTriggerGauche = 28;
+
+int SonarEchoAvant = 27;
+int SonarEchoArriere = 33;
+int SonarEchoDroite = 23;
+int SonarEchoGauche = 29;
+
+int rotationTime = 775;
+
+Servo servorgb;
+Servo servoarm;
+
+void setup() 
+{
   pinMode(SonarTriggerAvant, OUTPUT);
   pinMode(SonarTriggerArriere, OUTPUT);
   pinMode(SonarTriggerDroite, OUTPUT);
@@ -14,11 +30,16 @@ void setup(){
   pinMode(SonarEchoDroite, INPUT);
   pinMode(SonarEchoGauche, INPUT);
 
-  Serial.begin(9600);
-}
+  digitalWrite(SonarTriggerAvant, LOW);
+  digitalWrite(SonarTriggerArriere, LOW);
+  digitalWrite(SonarTriggerDroite, LOW);
+  digitalWrite(SonarTriggerGauche, LOW);
 
-void loop(){
+  
+  servorgb.attach(11);
+  servoarm.attach(12);
+} 
 
-	parking()
-
-}
+void loop() {
+  
+} 
