ef3d7c85ae9dc4dc926338c3b6ffd9482a710670
Unknown <bapt890@gmail.com>
2017-06-24 20:02:50 +0200

    maze

--- maze.h
+++ maze.h
@@ -2,14 +2,14 @@
 #include "motor.h"
 
 void maze(){
-  moteurstart(movementSpeed, motor);
-  delay(3000);
-  servoarm.write(90);
-  moteur.stop(1);
-  delay(rotationTime*2);
-  servoarm.write(0);
-  moteur.avancer(1, movementSpeed);
-  moteur.stop(2);
-  delay(rotationTime);
-  moteur.avancer(2);
+	moteurstart(movementSpeed, motor);
+	delay(3000);
+	servoarm.write(90);
+	moteur.stop(1);
+	delay(rotationTime*2);
+	servoarm.write(0);
+	moteur.avancer(1, movementSpeed);
+	moteur.stop(2);
+	delay(rotationTime);
+	moteur.avancer(2);
 }
