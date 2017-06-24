6ea4df44eca454b9f35387ee9c245754a5b3c669
Unknown <bapt890@gmail.com>
2017-06-24 14:37:58 +0200

    Update global/main

--- main.ino
+++ main.ino
@@ -1,36 +1,24 @@
 #include <Moteur.h>
+#include <parking.ino>
+#include <global.ino>
 
-//Définition des pins triggers et echos pour les sonars
-int SonarTriggerAvant = 26;
-int SonarTriggerArriere = 32;
-int SonarTriggerDroite = 22;
-int SonarTriggerGauche = 28;
+//Initialise le mode des pins
+void setup(){
+  pinMode(SonarTriggerAvant, OUTPUT);
+  pinMode(SonarTriggerArriere, OUTPUT);
+  pinMode(SonarTriggerDroite, OUTPUT);
+  pinMode(SonarTriggerGauche, OUTPUT);
 
-int SonarEchoAvant = 27;
-int SonarEchoArriere = 33;
-int SonarEchoDroite = 23;
-int SonarEchoGauche = 29;
+  pinMode(SonarEchoAvant, INPUT);
+  pinMode(SonarEchoArriere, INPUT);
+  pinMode(SonarEchoDroite, INPUT);
+  pinMode(SonarEchoGauche, INPUT);
 
-//Définis les variables de vitesse (mouvement de base et rotation)
-char movementSpeed = 200;
-char rotateSpeed = 255;
-
-//Permet d'utiliser les donnés d'un sonar grâce à SonarTrigger--- et SonarEcho--- en paramètre
-int getSonarDistance(int trigger, int echo){
-  int mm;
-  digitalWrite(trigger, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger, LOW);
-  mm = pulseIn(echo, HIGH)/5.8;
-  return mm;
+  Serial.begin(9600);
 }
 
-//Permet de récupérer simultanément la distance de tout les sonars pour déterminer la position
-int getPos(){
-  int mmAvant, mmArriere, mmDroite, mmGauche, mmList[3];
-  mmAvant = getSonarDistance(SonarTriggerAvant, SonarEchoAvant);
-  mmArriere = getSonarDistance(SonarTriggerArriere, SonarEchoArriere);
-  mmDroite = getSonarDistance(SonarTriggerDroite, SonarEchoDroite);
-  mmGauche = getSonarDistance(SonarTriggerGauche, SonarEchoGauche);
-  return mmAvant, mmArrieren, mmDroite, mmGauche;
+void loop(){
+
+	parking()
+
 }
